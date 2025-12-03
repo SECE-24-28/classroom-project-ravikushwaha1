f6fadce96c0efbd62396a5f2abd5fdf7cb2edecc
Sreesairam V <sairam2381@users.noreply.github.com>
2025-12-03 12:32:40 +0530

    The updated code

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import "./App.css";
 import FlexBoxComponent from "./flex-box/flex-box-component";
 import HelloWorldComponent from "./hello-world/hello-world-component";
 import LeadersTomorrowComponent from "./leaders-tomorrow/leaders-tomorrow-component";
+import SymbolOfExcellenceComponent from "./symbol-of-excellence/symbol-of-execllence-component";
 
 function App() {
   return (
     <div>
       {/* <HelloWorldComponent /> */}
       {/* <FlexBoxComponent /> */}
-      <LeadersTomorrowComponent />
+      {/* <LeadersTomorrowComponent /> */}
+      <SymbolOfExcellenceComponent />
     </div>
   );
 }
--- src/symbol-of-excellence/symbol-of-excellence-style.js
+++ src/symbol-of-excellence/symbol-of-excellence-style.js
@@ -1,2 +1,38 @@
 import styled from "styled-components";
-export const SymbolOfExcellenceStyle = styled.div``;
+export const SymbolOfExcellenceStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  .para {
+    max-width: 1500px;
+    text-align: center;
+  }
+  .element__two {
+    display: flex;
+    max-width: 1280px;
+  }
+  .title {
+    text-align: center;
+    font-weight: 600;
+    font-size: 48px;
+  }
+  .para {
+    font-size: 20px;
+    font-weight: 500;
+    line-height: 1.8;
+    font-family: "Work Sans", sans-serif;
+    text-align: center;
+    color: rgb(86, 102, 131);
+  }
+  .sub__title {
+    color: rgb(243, 176, 79);
+    // border-left: 4px solid rgb(243, 176, 79);
+    padding-left: 0.5rem;
+  }
+  .details {
+    font-size: 18px;
+    font-weight: 500;
+    line-height: 1.8;
+    color: rgb(86, 102, 131);
+  }
+`;
