4b7f51db65fdf4c6c8735416bee5c0adbc17dc09
Sreesairam V <sairam2381@users.noreply.github.com>
2025-12-03 09:20:38 +0530

    Leaders tomorrow

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import FlexBoxComponent from "./flex-box/flex-box-component";
 import HelloWorldComponent from "./hello-world/hello-world-component";
+import LeadersTomorrowComponent from "./leaders-tomorrow/leaders-tomorrow-component";
 
 function App() {
   return (
     <div>
       {/* <HelloWorldComponent /> */}
-      <FlexBoxComponent />
+      {/* <FlexBoxComponent /> */}
+      <LeadersTomorrowComponent />
     </div>
   );
 }
