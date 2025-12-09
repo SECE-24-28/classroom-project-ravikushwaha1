b2d2ff9b3b81246b5788a52fe109a59bef2d775b
Sreesairam V <sairam2381@users.noreply.github.com>
2025-12-09 09:43:29 +0530

    Async java script

--- src/async-folder/index.js
+++ src/async-folder/index.js
@@ -6,9 +6,32 @@
 
 //Syncronous
 
-console.log("Before");
+// console.log("Before");
 
-setTimeout(() => {
-  console.log("Sreesairam");
-}, 5000);
-console.log("After");
+// setTimeout(() => {
+//   console.log("Sreesairam");
+// }, 5000);
+// console.log("After");
+// https://jsonplaceholder.typicode.com/users
+
+//  async function fetchApi(){
+//     const response=await fetch("https://jsonplaceholder.typicode.com/users");
+//     const ans=await response.json();
+//     console.log("The user:",ans);
+
+// }
+// fetchApi();
+import axios from "axios";
+
+//  async function fetchApi() {
+//    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//    const ans = await response.json();
+//    console.log("The user:", ans);
+//  }
+//  fetchApi();
+
+async function fetchApi() {
+  const response = await axios("https://jsonplaceholder.typicode.com/users");
+  console.log("The user:", response);
+}
+fetchApi();
