099892259717b00eb1ec204f8d11e9c174c5a899
Sreesairam V <sairam2381@users.noreply.github.com>
2025-12-09 09:13:03 +0530

    Async

---
+++ src/async-folder/index.js
@@ -0,0 +1,14 @@
+// console.log("Start");
+// for (let i = 0; i < 1e9; i++) {
+//   console.log("Hello  world");
+// }
+// console.log("End");
+
+//Syncronous
+
+console.log("Before");
+
+setTimeout(() => {
+  console.log("Sreesairam");
+}, 5000);
+console.log("After");
