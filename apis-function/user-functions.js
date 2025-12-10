44c96a9740806bc6bd008ff14301cca795acec14
Sreesairam V <sairam2381@users.noreply.github.com>
2025-12-10 15:39:04 +0530

    Done with the backend

--- apis-function/user-functions.js
+++ apis-function/user-functions.js
@@ -34,3 +34,36 @@ exports.createManyUsers = async (req, res) => {
     });
   }
 };
+exports.getAllUsers = async (req, res) => {
+  try {
+    const getDetails = await User.find();
+    // console.log("The users details:", getDetails);
+    return res.status(200).json({
+      success: true,
+      data: getDetails,
+    });
+  } catch (e) {
+    res.status(404).json({
+      success: false,
+      error: e,
+    });
+  }
+};
+exports.updateEmail = async (req, res) => {
+  try {
+    const { name, email } = req.body;
+    const updatethings = await User.updateOne(
+      { firstName: name },
+      { $set: { email: email } }
+    );
+    return res.status(200).json({
+      success: true,
+      message: "email is updated",
+    });
+  } catch (e) {
+    res.status(404).json({
+      success: false,
+      error: e,
+    });
+  }
+};
--- routes/basic-routes.js
+++ routes/basic-routes.js
@@ -2,8 +2,12 @@ const express = require("express");
 const {
   createUser,
   createManyUsers,
+  getAllUsers,
+  updateEmail,
 } = require("../apis-function/user-functions");
 const router = express.Router();
 router.post("/userSignup", createUser);
 router.post("/userSignupMany", createManyUsers);
+router.get("/getUsers", getAllUsers);
+router.put("/modifyMail", updateEmail);
 module.exports = router;
