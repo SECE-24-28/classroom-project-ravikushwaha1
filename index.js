92750f841261d0ded1abe6f4e60933c294734cef
Sreesairam V <sairam2381@users.noreply.github.com>
2025-12-10 15:42:10 +0530

    Pushing the latest code

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ const BasicRoutes = require("./routes/basic-routes");
 let port = 21000;
 const app = express();
 database.connect();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(express.json());
 app.use("/api/v1/User", BasicRoutes);
 app.get("/", (req, res) => {
